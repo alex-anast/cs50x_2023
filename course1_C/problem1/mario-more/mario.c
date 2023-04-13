f5ba074e47fda6bcf3b4ff07106a450c1ea26ba7
alex_anast <anastasioyaa@gmail.com>
2023-04-14 01:28:25 +0200

    add course 1 - problem 1 - mario more

--- course1_C/problem1/mario-more/mario.c
+++ course1_C/problem1/mario-more/mario.c
@@ -1,7 +1,48 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_pyramid(int size);
+
 int main(void) 
 {
-    
+    int size;
+    do
+    {
+        size = get_int("Give height of the pyramid: ");
+    }
+    /* condition that makes the input correct */
+    while (size < 1 || size > 8);
+
+    /* "pseudocode" */
+    print_pyramid(size);
+
+    return 0;
+}
+
+void print_pyramid(int size)
+{
+    /* for each line */
+    for (int i = 0; i < size; i++)
+    {
+        /**print spaces before hashes;
+         * it refers to the line number as reference
+         */
+        for (int j = 0; j < size - i - 1; j++) {
+            printf(" ");
+        }
+        
+        /* print hashes; again line no as reference */
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
+        /* print the two spaces between the hashes, fixed */
+        printf("  "); 
+        
+        /* print hashes again; same as above */
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
+        /* move to the next line, no need for extra spaces */
+        printf("\n"); 
+    }
 }
