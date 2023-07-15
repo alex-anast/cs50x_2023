b9c5ea7219f7bdfe5720bf36474cad476927d4e4
alex-anast <anastasioyaa@gmail.com>
2023-07-15 17:59:06 +0200

    change local repo

---
+++ course6_Python/problem6/sentimental-credit/credit.py
@@ -0,0 +1,14 @@
+def main():
+    # get the card number correctly
+    try:
+        # int handles by itself the lenght of the integer
+        card_number = int(input("Type your card number: "))
+    except:
+        ...
+
+    
+    
+
+if __name__ == '__main__':
+    main()
+
